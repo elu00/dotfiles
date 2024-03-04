25dd448554785811c534e94bef9234dd2c4e1adf
Ethan Lu <ethanlu@stanford.edu>
2024-03-03 17:33:41 -0800

    Fully disable treesitter for tex?

--- nvim/lua/plugins/vimtex.lua
+++ nvim/lua/plugins/vimtex.lua
@@ -4,9 +4,10 @@ return {
 		"nvim-treesitter/nvim-treesitter",
 		opts = function(_, opts)
 			opts.ignore_install = { "latex" }
-			if type(opts.ensure_installed) == "table" then
+			--[[if type(opts.ensure_installed) == "table" then
 				vim.list_extend(opts.ensure_installed, { "bibtex", "latex" })
 			end
+            --]]
 			if type(opts.highlight.disable) == "table" then
 				vim.list_extend(opts.highlight.disable, { "latex" })
 			else
