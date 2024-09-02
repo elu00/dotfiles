3793076ccae6ac45790913ccefeda214022511aa
Ethan Lu <elu@nevada.unr.edu>
2024-09-01 20:47:38 -0700

    Native luarocks

--- nvim/lua/plugins/neorg.lua
+++ nvim/lua/plugins/neorg.lua
@@ -1,13 +1,7 @@
 return {
 	{
-		"vhyrro/luarocks.nvim",
-		priority = 1000, -- We'd like this plugin to load first out of the rest
-		config = true, -- This automatically runs `require("luarocks-nvim").setup()`
-	},
-	{
 		"nvim-neorg/neorg",
 		-- tag = "*",
-		dependencies = { "luarocks.nvim" },
 		--version = "v7.0.0",
 		config = function()
 			require("neorg").setup({
