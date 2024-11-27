986e49fbce4e70fd1c923adaa5c6907635390782
Ethan Lu <elu@nevada.unr.edu>
2024-11-27 00:09:03 -0800

    Update lazy config?

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -1,18 +1,6 @@
 return {
 	{
 		"saghen/blink.cmp",
-		dependencies = {
-			-- add blink.compat to dependencies
-			{ "saghen/blink.compat", version = "event", opts = {
-				enable_events = true,
-			} },
-			{
-				"Exafunction/codeium.nvim",
-				cmd = "Codeium",
-				build = ":Codeium Auth",
-				opts = {},
-			},
-		},
 		opts = {
 			keymap = {
 				preset = "enter",
@@ -28,26 +16,6 @@ return {
 					"fallback",
 				},
 				["<S-Tab>"] = { "select_prev", "snippet_backward", "fallback" },
-			},
-			sources = {
-				completion = {
-					enabled_providers = { "lsp", "path", "snippets", "buffer", "codeium" },
-				},
-				providers = {
-					codeium = {
-						name = "codeium", -- IMPORTANT: use the same name as you would for nvim-cmp
-						module = "blink.compat.source",
-
-						-- all blink.cmp source config options work as normal:
-						score_offset = 3,
-						max_items = 4,
-
-						opts = {
-							-- options passed to the completion source
-							-- equivalent to `option` field of nvim-cmp source config
-						},
-					},
-				},
 			},
 			windows = {
 				autocomplete = {
